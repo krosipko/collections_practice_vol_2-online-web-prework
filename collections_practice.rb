db7ee2e64f4f65dfa7e8ec1f46975b1e09f991a9
Kyle Rosipko <krosipko@outlook.com>
2019-03-02 23:31:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
-# your code goes here
+def begins_with_r(args)
+  new_array = []
+  args.each do |ray|
+    if ray.chr == "r"
+      new_array << ray
+    end
+  end
+  if new_array.length == args.length
+    true
+  else
+    false
+  end
+end
+
+def contain_a(args)
+  new_array = args.select {|i| i.include?("a")}
+end
+
+
+def first_wa(args)
+  new_array = args.select {|x| /\Awa/.match(x)}
+  new_array[0]
+end
+
+def remove_non_strings(args)
+  args.delete_if {|x| x.class != String}
+end
+
+def count_elements(args)
+
+end
+=begin
+
+def first_wa(args)
+  new_element = 
+  args.each do  |element|
+    if /\Awa/.match(element)
+      new_element = element
+      break
+    end
+  end
+  new_element
+end
+=end
